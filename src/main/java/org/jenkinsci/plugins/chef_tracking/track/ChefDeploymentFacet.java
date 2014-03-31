abdb0f1c8e1cf87e2d16c0db532ac9cfbbdecd15
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-31 08:58:04 -0700

    minimum implementation

--- src/main/java/org/jenkinsci/plugins/chef_tracking/track/ChefDeploymentFacet.java
+++ src/main/java/org/jenkinsci/plugins/chef_tracking/track/ChefDeploymentFacet.java
@@ -2,11 +2,12 @@ package org.jenkinsci.plugins.chef_tracking.track;
 
 import hudson.model.Fingerprint;
 import org.jenkinsci.plugins.deployment.DeploymentFacet;
+import org.jenkinsci.plugins.deployment.HostRecord;
 
 /**
  * @author Kohsuke Kawaguchi
  */
-public class ChefDeploymentFacet extends DeploymentFacet {
+public class ChefDeploymentFacet extends DeploymentFacet<HostRecord> {
     public ChefDeploymentFacet(Fingerprint fingerprint, long timestamp) {
         super(fingerprint, timestamp);
     }
--- src/main/java/org/jenkinsci/plugins/chef_tracking/track/RootActionImpl.java
+++ src/main/java/org/jenkinsci/plugins/chef_tracking/track/RootActionImpl.java
@@ -1,11 +1,27 @@
 package org.jenkinsci.plugins.chef_tracking.track;
 
-import com.sun.deploy.net.HttpResponse;
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.Fingerprint;
+import hudson.model.RootAction;
+import hudson.util.HttpResponses;
+import hudson.util.IOUtils;
+import jenkins.model.FingerprintFacet;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.jenkinsci.plugins.deployment.DeploymentFacet;
+import org.jenkinsci.plugins.deployment.HostRecord;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 /**
- * Exposed at /puppet to receive report submissions from puppet over HTTP.
+ * Exposed at /puppet to receive report submissions from Chef over HTTP.
  *
  * @author Kohsuke Kawaguchi
  */
@@ -28,14 +44,61 @@ public class RootActionImpl implements RootAction {
      */
     @RequirePOST
     public HttpResponse doReport(StaplerRequest req) throws IOException {
-        // TODO: stapler YAML support
+        JSONObject o = JSONObject.fromObject(IOUtils.toString(req.getReader()));
 
-        PuppetReport.load(req.getReader()).process();
+        report(o);
 
         return HttpResponses.ok();
     }
 
+    public void report(JSONObject o) throws IOException {
+        Jenkins.getInstance().checkPermission(DeploymentFacet.RECORD);
+
+        String node = o.getString("node");
+        String env = o.getString("environment");
+        Date t = new Date(o.getString("end_time"));
+
+        for (JSONObject upd : (List<JSONObject>)(List)o.getJSONArray("updates")) {
+            String path = upd.optString("path");
+            String md5 = upd.optString("md5");
+
+            if (md5==null || path==null)    continue;   // defensive check
+
+            ChefDeploymentFacet f = getDeploymentFacet(md5);
+            if (f!=null) {
+                f.add(new HostRecord(t.getTime(), node, env, path, null));
+            }
+        }
+    }
+
     public static RootActionImpl get() {
         return Jenkins.getInstance().getExtensionList(RootAction.class).get(RootActionImpl.class);
+    }
+
+    /**
+     * Resolve {@link DeploymentFacet} to attach the record to, or null if there's none.
+     *
+     * This is a convenience method for subtypes.
+     */
+    protected ChefDeploymentFacet getDeploymentFacet(String md5) throws IOException {
+        if (md5 == null) return null;
+
+        Fingerprint f = Jenkins.getInstance().getFingerprintMap().get(md5);
+        if (f == null) return null;
+
+        Collection<FingerprintFacet> facets = f.getFacets();
+        ChefDeploymentFacet df = findDeploymentFacet(facets);
+        if (df == null) {
+            df = new ChefDeploymentFacet(f, System.currentTimeMillis());
+            facets.add(df);
+        }
+        return df;
+    }
+
+    private ChefDeploymentFacet findDeploymentFacet(Collection<FingerprintFacet> facets) {
+        for (ChefDeploymentFacet df : Util.filter(facets, ChefDeploymentFacet.class)) {
+            return df;
+        }
+        return null;
     }
 }
